bb24011dfd23612552a856d9226911eb998ea6f9
oaynuy1997 <96091717+oaynuy1997@users.noreply.github.com>
2021-12-13 20:45:14 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,10 @@
 This is a README for tfa project.
 
+The file Top10.ipynb shows the top 10 causes of calls to 311 in area with zipcode 10018. 
+
+The file Parking.ipynb shows whether illegal parking incidents are a larger fraction of
+total 311 incidents in area with zipcode 10028 than they are in general in the entire new york city.
+
 Section: 002
 
 Group Members:
